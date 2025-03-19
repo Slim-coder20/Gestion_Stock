2a97b58b139a6d2ee93006ba1fec7ae0f0a63483
slim <slimabida21@gmail.com>
2025-03-19 14:28:59 +0100

    création des entités Area , Article , Structure et modif dans la structure User

---
+++ src/Entity/Article.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?structure $structure = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStructure(): ?structure
+    {
+        return $this->structure;
+    }
+
+    public function setStructure(?structure $structure): static
+    {
+        $this->structure = $structure;
+
+        return $this;
+    }
+}
